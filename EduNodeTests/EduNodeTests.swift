a16f5efa441f2724075f15758ef614876d9be49d
EuanTop <euan@mail.bnu.edu.cn>
2026-02-15 16:02:58 +0800

    Initial Commit

---
+++ EduNodeTests/EduNodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  EduNodeTests.swift
+//  EduNodeTests
+//
+//  Created by Euan on 2/15/26.
+//
+
+import Testing
+@testable import EduNode
+
+struct EduNodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
